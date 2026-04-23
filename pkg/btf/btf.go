76d692a5563ae5744d7523a5c8dc35bc830c5435
Jiri Olsa <jolsa@kernel.org>
2026-04-23 16:45:26 +0200

    tetragon: Use longest failed chain for last error in BTF resolve
    Currently we break the resolve loop earlier than it's needed,
    which might cause premature end of the resolve scan, let's
    remove that.
    
    Let's instead store the last error based on the largest resolved
    depth and use this error as the final one in case of error.
    
    [1] 4231aba000f5 ("powerpc/64s: Fix page table fragment refcount race vs speculative references")
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- pkg/btf/btf.go
+++ pkg/btf/btf.go
@@ -6,6 +6,7 @@
 package btf
 
 import (
+	"errors"
 	"fmt"
 	"math"
 	"os"
@@ -301,6 +302,15 @@ func ResolveBTFPath(
 	}
 }
 
+type resolveError struct {
+	idx int
+	str string
+}
+
+func (e *resolveError) Error() string {
+	return e.str
+}
+
 func processMembers(
 	btfArgs *[api.MaxBTFArgDepth]api.ConfigBTFArg,
 	currentType btf.Type,
@@ -308,7 +318,7 @@ func processMembers(
 	pathToFound []string,
 	i int,
 ) (*btf.Type, error) {
-	var lastError error
+	var lastError *resolveError
 	memberWasFound := false
 	for _, member := range members {
 		if len(member.Name) == 0 { // If anonymous struct, fallthrough
@@ -316,14 +326,16 @@ func processMembers(
 			btfArgs[i].IsInitialized = uint16(1)
 			lastTy, err := ResolveBTFPath(btfArgs, ResolveNestedTypes(member.Type), pathToFound, i)
 			if err != nil {
-				if lastError != nil {
-					idx := i + 1
-					// If the error raised originates from a depth greater than the current one, we stop the search.
-					if idx < len(pathToFound) && strings.Contains(lastError.Error(), pathToFound[idx]) {
-						break
+				if err2, ok := errors.AsType[*resolveError](err); ok {
+					// Propagate the deepest error.
+					if lastError == nil || lastError.idx < err2.idx {
+						lastError = err2
 					}
+				} else if lastError == nil {
+					// Keep non-resolve error in the game, if there
+					// hasn't been any resolve one yet.
+					lastError = &resolveError{i, err.Error()}
 				}
-				lastError = err
 				continue
 			}
 			return lastTy, nil
@@ -344,12 +356,12 @@ func processMembers(
 		if lastError != nil {
 			return nil, lastError
 		}
-		return nil, fmt.Errorf(
+		return nil, &resolveError{i, fmt.Sprintf(
 			"attribute %q not found in structure %q found %v",
 			pathToFound[i],
 			currentType.TypeName(),
 			members,
-		)
+		)}
 	}
 	if t, ok := currentType.(*btf.Pointer); ok {
 		btfArgs[i].IsPointer = uint16(1)
